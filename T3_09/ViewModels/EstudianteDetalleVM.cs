2ca539b03bd438a11da7d976a73cb76aff46e603
HP <reylesruizr@gmail.com>
2026-06-11 22:55:20 -0500

    Recrear AccesoControllerTests en UTF-git push origin main

--- T3_09/ViewModels/EstudianteDetalleVM.cs
+++ T3_09/ViewModels/EstudianteDetalleVM.cs
@@ -1,17 +1,19 @@
-﻿namespace T3_09.ViewModels
+﻿using System;
+
+namespace T3_09.ViewModels
 {
     public class EstudianteDetalleVM
     {
         public int IdEstudiante { get; set; }
-        public string NombreCompleto { get; set; }
-        public string Dni { get; set; }
-        public string NombreApoderado { get; set; }
-        public string Direccion { get; set; }
+        public string? NombreCompleto { get; set; }
+        public string? Dni { get; set; }
+        public string? NombreApoderado { get; set; }
+        public string? Direccion { get; set; }
         public DateTime FechaNacimiento { get; set; }
 
-        public string SituacionMatricula { get; set; }    
+        public string? SituacionMatricula { get; set; }    
 
-        public string GradoSeccion { get; set; } 
+        public string? GradoSeccion { get; set; } 
         // Ej: "5to - A"
     }
 }
